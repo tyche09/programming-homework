994d32d6642fb14b2c35cf34d83c19e2733cf583
tyche09 <tyche0920@qq.com>
2024-12-04 15:28:03 +0800

    prime number

---
+++ main10.c
@@ -0,0 +1,39 @@
+//guess prime number
+#include <stdio.h>
+#include <math.h>
+int judgePrime(int);
+int main()
+{
+	int n;
+	while (scanf("%d", &n) != EOF) { //多组数据输入的基本框架
+		printf(judgePrime(n) ? "yes\n" : "no\n"); // 用条件表达式使表述更为精炼
+	}
+	return 0;
+}
+
+int judgePrime(int n) { //函数完成判断一个数是否为素数
+	/**********Begin**********/
+
+ int flag=1;
+ if(n<=1){
+   return 0;
+ }
+ else{
+     for(int i=2;i<n;i++){
+         if(n%i==0){
+
+            flag=0;
+            break;
+            return 0;
+         }
+     }
+     if(flag==1){
+        return 1;
+     }
+ }
+
+
+
+
+    /**********End**********/
+}
